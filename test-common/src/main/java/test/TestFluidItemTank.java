caf54132fd7ae52998322648f68550bb855dfc51
Liven <56485712+CallMeAryan@users.noreply.github.com>
2024-11-07 19:05:14 +0530

    implemented ItemFluidTank in TestFluidItemTank

--- test-common/src/main/java/test/TestFluidItemTank.java
+++ test-common/src/main/java/test/TestFluidItemTank.java
@@ -1,20 +1,19 @@
 package test;
 
 import com.absolutelyaryan.fluid.BaseFluidTank;
+import com.absolutelyaryan.fluid.ItemFluidTank;
 import com.absolutelyaryan.fluid.UniversalFluidTank;
 import com.absolutelyaryan.providers.FluidProvider;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class TestFluidItemTank extends Item implements FluidProvider.ITEM {
-    UniversalFluidTank tank;
     public TestFluidItemTank(Properties properties) {
         super(properties);
-        tank = new BaseFluidTank(1000);
     }
 
     @Override
     public UniversalFluidTank getFluidTank(ItemStack stack) {
-        return tank;
+        return new ItemFluidTank(stack, 1000);
     }
 }
