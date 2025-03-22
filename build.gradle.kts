27370fa242419bf592a4c28c7258c998dbb27f1b
Fej1Dev <guymoshegg007@gmail.com>
2025-03-22 21:54:53 +0200

    update kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.google.gson.Gson
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 
 plugins {
@@ -6,6 +7,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
     id("architectury-plugin") version "3.4-SNAPSHOT"
+    id("me.modmuss50.mod-publish-plugin") version "0.8.4"
 }
 
 architectury {
@@ -17,6 +19,7 @@ subprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "dev.architectury.loom")
     apply(plugin = "architectury-plugin")
+    apply(plugin = "me.modmuss50.mod-publish-plugin")
 
     val minecraft_version: String by project
     val archives_name = rootProject.name
@@ -96,5 +99,112 @@ subprojects {
 
     java {
         withSourcesJar()
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+
+
+//    publishing {
+//        publications {
+//            mavenJava(MavenPublication) {
+//                groupId "com.fej1fun.potentials"
+//                artifactId "potentials-$modLoader"
+//                version project.version
+//                        from components.java
+//                        pom {
+//                            name = "Potentials" + modLoader.capitalize()
+//                            url = "https://github.com/Fej1Dev/Potentials"
+//                        }
+//            }
+//        }
+//
+//        // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+//        repositories {
+//            maven {
+//                name = "GithubPackages"
+//                url = "https://maven.pkg.github.com/Fej1Dev/Potentials"
+//                credentials {
+//                    username = System.getenv("GITHUB_ACTOR")
+//                    password = System.getenv("GITHUB_TOKEN")
+//                }
+//            }
+//            maven {
+//                name = "ExodusStudio"
+//                url = "https://maven.exodusstudio.org/releases"
+//                credentials {
+//                    username = System.getenv("MAVEN_USER")
+//                    password = System.getenv("MAVEN_PASS")
+//                }
+//            }
+//        }
+//    }
+
+    if (!isCommon) {
+        publishMods {
+
+            val supported_minecraft_versions : String by project
+            val parsedJson : Versions = Gson().fromJson(supported_minecraft_versions, Versions::class.java)
+            val supportedVersions : Array<String> = parsedJson.versions
+            val mod_version : String by project
+
+            file = file("../${modLoader.capitalize()}/build/libs/potentials-$modLoader-$minecraft_version-$mod_version.jar")
+            changelog = file("../changelog.md").readText(charset = charset("UTF-8"))
+
+            val release_type : String by project
+            if (release_type == "alpha")
+                type = ALPHA
+            else if (release_type == "beta")
+                type = BETA
+            else
+                type = STABLE
+
+            modLoaders.add(modLoader)
+            displayName = "[${modLoader.capitalize()} ${minecraft_version}] Potentials ${mod_version}"
+
+            curseforge {
+                projectId = "1165185"
+                projectSlug = "potentials" // Required for discord webhook
+                accessToken = providers.gradleProperty("CURSEFORGE_API_KEY")
+                for (version in supportedVersions)
+                    minecraftVersions.add(version)
+
+                javaVersions.add(JavaVersion.VERSION_21)
+                requires("architectury-api")
+                if (modLoader == "fabric") {
+                    requires("fabric-api")
+                }
+            }
+
+            modrinth {
+                projectId = "J9pKOkxP"
+                accessToken = providers.gradleProperty("MODRINTH_TOKEN")
+                for (version in supportedVersions)
+                    minecraftVersions.add(version)
+
+                requires("architectury-api")
+                if (modLoader == "fabric") {
+                    requires("fabric-api")
+                }
+
+            }
+
+//        github {
+//            repository = "Fej1Dev/Potentials"
+//            accessToken = providers.gradleProperty("GITHUB_TOKEN")
+//            commitish = "main"
+//            tagName = "${type}/${minecraft_version}"
+//
+//            // Upload the files to a previously created release, by providing another github publish task
+//            // This is useful in multi-project builds where you want to publish multiple subprojects to a single release
+//            parent project(":").tasks.named("publishGithub")
+//
+//            // Optionally allow the release to be created without any attached files.
+//            // This is useful when you have subprojects using the parent option that you want to publish a single release.
+//            allowEmptyFiles = true
+//        }
+        }
+    }
+
 }
+
+data class Versions(val versions: Array<String>)
